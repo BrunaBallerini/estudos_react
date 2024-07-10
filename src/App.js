d1a8177279c633b8515af3f900f94571845c244f
BrunaBallerini <brunaballerini@gmail.com>
2024-07-10 19:18:16 -0300

    State com array de objetos e map

--- src/App.js
+++ src/App.js
@@ -1,53 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.handlePClique = this.handlePClique.bind(this);
-  //   this.state = {
-  //     name: "Bruna ",
-  //     counter: 0
-  //   };
-  // }
-
   state = {
-    name: "Bruna ",
-    counter: 0
+    posts: [
+      {
+        id: 1,
+        title: 'Título 1',
+        body: 'Texto do post 1',
+      },
+      {
+        id: 2,
+        title: 'Título 2',
+        body: 'Texto do post 2',
+      },
+      {
+        id: 2,
+        title: 'Título 2',
+        body: 'Texto do post 2',
+      }
+    ]
   };
 
-  handlePClique = () => {
-    const { name } = this.state;
-    console.log(`<p> clicado ${name}`);
-    this.setState({ name: 'Ballerini ' });
-  }
-
-  handleAClique = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
-  }
-
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClique}>
-            {name} {counter}
-          </p>
-          <a onClick={this.handleAClique}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     )
   }
