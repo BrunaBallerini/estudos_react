96efc4a3b45d291e5ba5c401d4cb532a89a58542
BrunaBallerini <brunaballerini@gmail.com>
2024-07-16 08:39:11 -0300

    Data fetching

--- src/App.js
+++ src/App.js
@@ -4,60 +4,34 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-    counter: 0,
-    posts: [
-      {
-        id: 1,
-        title: 'Título 1',
-        body: 'Texto do post 1',
-      },
-      {
-        id: 2,
-        title: 'Título 2',
-        body: 'Texto do post 2',
-      },
-      {
-        id: 2,
-        title: 'Título 2',
-        body: 'Texto do post 2',
-      }
-    ]
+    posts: []
   };
-  timeoutUpdate = null;
 
   componentDidMount() {
-    this.handleTimeout();
+    this.loadPosts();
   }
 
-  componentDidUpdate() {
-    this.handleTimeout();
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const [posts] = await Promise.all([postsResponse]);
+    const postsJson = await posts.json();
+    this.setState({ posts: postsJson });
   }
 
-  componentWillUnmount() {
-    clearTimeout(this.timeoutUpdate);
-  }
-
-  handleTimeout = () => {
-    const { posts, counter } = this.state;
-    posts[0].title = 'O título 1 mudou';
-
-    this.timeoutUpdate = setTimeout(() => {
-      this.setState({ posts, counter: counter + 1 });
-    }, 1000);
-  }
 
   render() {
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
     return (
-      <div className="App">
-        {counter}
-        {posts.map(post => (
-          <div key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className='container'>
+        <div className="posts">
+          {posts.map(post => (
+            <div key={post.id} className='post'>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </div>
+          ))}
+        </div>
+      </section>
     )
   }
 }
